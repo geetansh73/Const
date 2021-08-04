6f162056d9f77350b7ccd288d43d4db393619d24
Geetansh Gupta <67698473+geetansh73@users.noreply.github.com>
2021-08-04 12:19:47 +0530

    Create const.java

---
+++ const.java
@@ -0,0 +1,19 @@
+class constructor
+{
+int n,i,sum;
+constructor()
+{
+}
+constructor (int n)
+{
+for(i=0;i<=n;i++)
+{
+sum=sum+i;
+}
+System.out.print(sum);
+}
+public static void main(String geetu[])
+{
+constructor c=new constructor(20);
+}
+}
